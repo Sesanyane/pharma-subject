0105c425e7400fdca057d5a46e4a084db8831585
tumiemosweu <imosweuk@gmail.com>
2021-01-18 11:13:35 +0200

    remove next form getter

--- pharma_subject/admin/modeladmin_mixins.py
+++ pharma_subject/admin/modeladmin_mixins.py
@@ -1,6 +1,5 @@
 from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
 from edc_base.sites.admin import ModelAdminSiteMixin
-from edc_metadata import NextFormGetter
 from edc_model_admin import (
     ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsMixin,
     ModelAdminFormAutoNumberMixin, ModelAdminAuditFieldsMixin,
@@ -19,4 +18,3 @@ class ModelAdminMixin(ModelAdminNextUrlRedirectMixin,
     list_per_page = 10
     date_hierarchy = 'modified'
     empty_value_display = '-'
-    next_form_getter_cls = NextFormGetter
